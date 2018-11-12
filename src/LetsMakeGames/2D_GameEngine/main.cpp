08b0136b3fc071af0c42f1c1ce0bf5187ba8d541
kstandbridge <kstandbridge@blizzard.com>
2018-11-12 11:20:43 +0000

    SDL2 - Making the game loop

---
+++ src/LetsMakeGames/2D_GameEngine/main.cpp
@@ -0,0 +1,30 @@
+#define SDL_MAIN_HANDLED
+#include "SDL2/SDL.h"
+#include "Game.h"
+
+Game* game = nullptr;
+
+int main(int argc, char* argv[])
+{
+	game = new Game();
+
+	game->init("BirchEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+	while (game->running())
+	{
+		// handle any user input
+		game->handleEvents();
+		// update all object eh, position, etc
+		game->update();
+		// render changes to the display
+		game->render();
+	}
+
+	game->clean();
+
+	delete game;
+
+	system("PAUSE");
+
+	return 0;
+}
