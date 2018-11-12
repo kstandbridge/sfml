b3a0b138eff3444b3d1f2c4bd150d746189f38d0
kstandbridge <kstandbridge@blizzard.com>
2018-11-12 15:56:02 +0000

    The game object class

--- src/LetsMakeGames/2D_GameEngine/Game.cpp
+++ src/LetsMakeGames/2D_GameEngine/Game.cpp
@@ -1,9 +1,8 @@
 #include "Game.h"
-#include "TextureManager.h"
+#include "GameObject.h"
 
-SDL_Texture* playerTex;
-SDL_Rect srcR, destR;
-
+GameObject* player;
+GameObject* enemy;
 
 Game::Game()
 {
@@ -35,7 +34,8 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		isRunning = true;
 	}
 
-	playerTex = TextureManager::LoadTexture("assets/mushroom.png", renderer);
+	player = new GameObject("assets/mushroom.png", renderer, 0, 0);
+	enemy = new GameObject("assets/1UP_mushroom.png", renderer, 50, 50);
 }
 
 void Game::handleEvents()
@@ -55,18 +55,15 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	cnt++;
-	destR.h = 64;
-	destR.w = 64;
-	destR.x = cnt;
-
-	std::cout << cnt << std::endl;
+	player->Update();
+	enemy->Update();
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
+	player->Render();
+	enemy->Render();
 	SDL_RenderPresent(renderer);
 }
 
