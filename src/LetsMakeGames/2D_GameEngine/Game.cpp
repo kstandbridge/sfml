bb850d71caf6db3112709b56fc64613a1bb3fbf3
kstandbridge <kstandbridge@blizzard.com>
2018-11-12 12:10:56 +0000

    drawing textures/sprites to the screen

--- src/LetsMakeGames/2D_GameEngine/Game.cpp
+++ src/LetsMakeGames/2D_GameEngine/Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
 
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
 
 Game::Game()
 {
@@ -22,24 +24,18 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		std::cout << "Subsystems Initialized!..." << std::endl;
 
 		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if(window)
-		{
-			std::cout << "Window created!" << std::endl;
-		}
-
 		renderer = SDL_CreateRenderer(window, -1, 0);
 		if(renderer)
 		{
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			std::cout << "Renderer created!" << std::endl;
 		}
 
 		isRunning = true;
 	}
-	else
-	{
-		isRunning = false;
-	}
+
+	SDL_Surface* tmpSurface = IMG_Load("assets/mushroom.png");
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents()
@@ -60,13 +56,17 @@ void Game::handleEvents()
 void Game::update()
 {
 	cnt++;
+	destR.h = 64;
+	destR.w = 64;
+	destR.x = cnt;
+
 	std::cout << cnt << std::endl;
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	// this is where we would add stuff to render;
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
 	SDL_RenderPresent(renderer);
 }
 
