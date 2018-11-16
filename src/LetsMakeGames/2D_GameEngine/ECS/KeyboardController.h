3fca2a7d98737f609569a48591900178f0adf943
kstandbridge <kstandbridge@blizzard.com>
2018-11-16 10:14:51 +0000

    Multiple animations and render flipping

--- src/LetsMakeGames/2D_GameEngine/ECS/KeyboardController.h
+++ src/LetsMakeGames/2D_GameEngine/ECS/KeyboardController.h
@@ -8,10 +8,13 @@ class KeyboardController : public Component
 {
 public:
 	TransformComponent* transform;
+	SpriteComponent* sprite;
+
 
 	void init() override
 	{
 		transform = &entity->getComponent<TransformComponent>();
+		sprite = &entity->getComponent<SpriteComponent>();
 	}
 
 	void update() override
@@ -20,10 +23,10 @@ public:
 		{
 			switch (Game::event.key.keysym.sym)
 			{
-			case SDLK_w: transform->velocity.y = -1.0f; break;
-			case SDLK_a: transform->velocity.x = -1.0f; break;
-			case SDLK_s: transform->velocity.y =  1.0f; break;
-			case SDLK_d: transform->velocity.x =  1.0f; break;
+			case SDLK_w: transform->velocity.y = -1.0f; sprite->Play("Walk"); break;
+			case SDLK_a: transform->velocity.x = -1.0f; sprite->Play("Walk"); sprite->spriteFlip = SDL_FLIP_HORIZONTAL; break;
+			case SDLK_s: transform->velocity.y =  1.0f; sprite->Play("Walk"); break;
+			case SDLK_d: transform->velocity.x =  1.0f; sprite->Play("Walk"); break;
 			default: break;
 			}
 		}
@@ -32,10 +35,10 @@ public:
 		{
 			switch (Game::event.key.keysym.sym)
 			{
-			case SDLK_w: transform->velocity.y = 0.0f; break;
-			case SDLK_a: transform->velocity.x = 0.0f; break;
-			case SDLK_s: transform->velocity.y = 0.0f; break;
-			case SDLK_d: transform->velocity.x = 0.0f; break;
+			case SDLK_w: transform->velocity.y = 0.0f; sprite->Play("Idle"); break;
+			case SDLK_a: transform->velocity.x = 0.0f; sprite->Play("Idle"); sprite->spriteFlip = SDL_FLIP_NONE; break;
+			case SDLK_s: transform->velocity.y = 0.0f; sprite->Play("Idle"); break;
+			case SDLK_d: transform->velocity.x = 0.0f; sprite->Play("Idle"); break;
 			default: break;
 			}
 		}
--- src/LetsMakeGames/2D_GameEngine/ECS/SpriteComponent.h
+++ src/LetsMakeGames/2D_GameEngine/ECS/SpriteComponent.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "Components.h"
-#include "SDL2/SDL.h"
 #include "../TextureManager.h"
+#include "Animation.h"
+
+#include <SDL2/SDL.h>
+#include <map>
 
 class SpriteComponent : public Component
 {
@@ -15,17 +18,30 @@ private:
 	int speed = 100;
 
 public:
+
+	int animIndex = 0;
+	std::map<const char*, Animation> animations;
+
+	SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
+
 	SpriteComponent() = default;
 	SpriteComponent(const char* path)
 	{
 		setTex(path);
 	}
 
-	SpriteComponent(const char* path, int nFrames, int mSpeed)
+	SpriteComponent(const char* path, bool isAnimated)
 	{
-		animated = true;
-		frames = nFrames;
-		speed = mSpeed;
+		animated = isAnimated;
+
+		Animation idle = Animation(0, 3, 100);
+		Animation walk = Animation(1, 8, 100);
+
+		animations.emplace("Idle", idle);
+		animations.emplace("Walk", walk);
+
+		Play("Idle");
+
 		setTex(path);
 	}
 
@@ -55,6 +71,8 @@ public:
 			srcRect.x = srcRect.w * static_cast<int>((SDL_GetTicks() / speed) % frames);
 		}
 
+		srcRect.y = animIndex * transform->height;
+
 		destRect.x = static_cast<int>(transform->position.x);
 		destRect.y = static_cast<int>(transform->position.y);
 		destRect.w = transform->width * transform->scale;
@@ -63,8 +81,14 @@ public:
 
 	void draw() override
 	{
-		TextureManager::Draw(texture, srcRect, destRect);
+		TextureManager::Draw(texture, srcRect, destRect, spriteFlip);
 	}
-	
+
+	void Play(const char* animName)
+	{
+		frames = animations[animName].frames;
+		animIndex = animations[animName].index;
+		speed = animations[animName].speed;
+	}
 };
 
